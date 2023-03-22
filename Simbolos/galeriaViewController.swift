09bce063394fa562d51aff98b75df5ee0b3ed267
AlekseyOberstein <420035969@pcpuma.acatlan.unam.mx>
2023-03-22 13:32:29 -0600

    Hice una aplicación con una galería básica.

---
+++ Simbolos/galeriaViewController.swift
@@ -0,0 +1,55 @@
+//
+//  galeriaViewController.swift
+//  Simbolos
+//
+//  Created by CEDAM17 on 22/03/23.
+//
+
+import UIKit
+
+class galeriaViewController: UIViewController {
+    
+    
+    @IBOutlet weak var galeria: UIImageView!
+    
+    
+    let images9 = ["death", "fool", "magician", "hierophant", "emperor", "priestess", "empress", "strenght", "lovers", "world"]
+    
+    var Pimagen = 0
+    
+    var contador = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    @IBAction func press(_ sender: Any) {
+        
+        Actualizar()
+        
+    }
+    
+    
+    func Actualizar(){
+        
+        
+        contador = Int.random(in: 0...9)
+        
+        let Pimagen = images9[contador]
+
+        let laImagen = UIImage(named: Pimagen)
+        
+        galeria.image = laImagen
+        
+        
+        
+        
+        
+    }
+    
+
+}
